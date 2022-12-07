d173c64a19ecb75427474dcf7795a52f9b3794a7
rafasu4 <raf59101@gmail.com>
2022-12-07 09:46:39 +0200

    ✅possib;e_winners

--- test_performance/test_consensus_under_deadline.py
+++ test_performance/test_consensus_under_deadline.py
@@ -12,16 +12,6 @@ class TestConsensusUnderDeadline(unittest.TestCase):
         t = 2
         self.cud = ConsensusUnderDeadline(voters=v, voters_type = v_type, alternatives=alters, default_alternative=df_alter,voters_preferences=vp, voters_current_ballot=v_cur_ballot, remaining_rounds=t)
     
-    def test_deploy_algorithm_results(self) -> None:
-        self.assertEqual(self.cud.deploy_algorithm(), 'b') # classic example
-        self.cud.remaining_rounds = 0
-        self.assertEqual(self.cud.deploy_algorithm(), 'null') # test case with no time to change the vote
-        self.cud.voters_current_ballot = {1: 'a', 2:'a', 3:'a'}
-        self.assertEqual(self.cud.deploy_algorithm(), 'a') # test case with unanimously on the start
-        self.cud.remaining_rounds = 3
-        self.cud.voters_preferences =[['a', 'b', 'c'], ['b', 'a', 'c'],['c', 'a', 'c']]
-        self.assertEqual(self.cud.deploy_algorithm(), 'a') # voters have same pretty close preference
-    
     def test_init_args(self) -> None:
         v = (1, 2, 3) 
         v_type = (1, 2, 3)
@@ -55,5 +45,40 @@ class TestConsensusUnderDeadline(unittest.TestCase):
         with self.assertRaises(ValueError):
             self.cud = ConsensusUnderDeadline(remaining_rounds=-1, voters=(1), voters_type = (0, 1), alternatives=alters, default_alternative=df_alter,voters_preferences=vp, voters_current_ballot=v_cur_ballot)
             self.cud = ConsensusUnderDeadline(remaining_rounds=-3, voters=(1), voters_type = (0, 1), alternatives=alters, default_alternative=df_alter,voters_preferences=vp, voters_current_ballot=v_cur_ballot)
+        
+    def test_deploy_algorithm_results(self) -> None:
+        self.assertEqual(self.cud.deploy_algorithm(), 'b') # classic example
+        self.cud.remaining_rounds = 0
+        self.assertEqual(self.cud.deploy_algorithm(), 'null') # test case with no time to change the vote
+        self.cud.voters_current_ballot = {1: 'a', 2:'a', 3:'a'}
+        self.assertEqual(self.cud.deploy_algorithm(), 'a') # test case with unanimously on the start
+        self.cud.remaining_rounds = 3
+        self.cud.voters_preferences =[['a', 'b', 'c'], ['b', 'a', 'c'],['c', 'a', 'c']]
+        self.assertEqual(self.cud.deploy_algorithm(), 'a') # voters have same pretty close preference
+
+    def test_possible_winners(self) -> None:
+        self.assertEqual(self.cud.possible_winners() ,['a', 'b', 'c'])
+        v = (1, 2, 3)
+        v_type = (1, 1, 1)
+        alters = ('a', 'b', 'c')
+        df_alter = 'null'
+        v_cur_ballot = {1: 'a', 2:'b', 3:'c'}
+        vp =[['a', 'b', 'c'], ['b', 'c', 'a'],['c', 'a', 'b']]
+        t = 1
+        self.cud = ConsensusUnderDeadline(remaining_rounds=t,voters=v, voters_type = v_type, alternatives=alters, default_alternative=df_alter,voters_preferences=vp, voters_current_ballot=v_cur_ballot)
+        self.assertEqual(self.cud.possible_winners() ,[])
+        self.cud = ConsensusUnderDeadline(remaining_rounds=4,voters=(1, 2, 3, 4, 5), voters_type = (0, 0, 0, 0, 0), alternatives=('a', 'b', 'c', 'd'), default_alternative=df_alter,voters_preferences={1: 'a', 2:'a', 3:'b', 4:'b', 5:'c' }, voters_current_ballot=v_cur_ballot)
+        v = (1, 2, 3, 4, 5)
+        v_type = (0, 0, 0, 0, 0)
+        alters = ('a', 'b', 'c', 'd')
+        v_cur_ballot = {1: 'a', 2:'a', 3:'b', 4:'b', 5:'c' }
+        vp =[['a', 'b', 'c', 'd'], ['a', 'c', 'b', 'd'], ['b', 'c', 'a', 'd'], ['b', 'a', 'c', 'd'], ['c', 'b', 'd', 'a']]
+        t = 4
+        self.cud = ConsensusUnderDeadline(remaining_rounds=1,voters=v, voters_type = v_type, alternatives=alters, default_alternative=df_alter,voters_preferences=vp, voters_current_ballot=v_cur_ballot)
+        self.assertEqual(self.cud.possible_winners() ,['a', 'b', 'c'])
+        t = 3
+        self.cud = ConsensusUnderDeadline(remaining_rounds=1,voters=v, voters_type = v_type, alternatives=alters, default_alternative=df_alter,voters_preferences=vp, voters_current_ballot=v_cur_ballot)
+        self.assertEqual(self.cud.possible_winners() ,['a', 'b'])
+        
 if __name__ == '__main__':
     unittest.main()
